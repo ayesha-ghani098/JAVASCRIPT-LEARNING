87da887aad59d64ee05d6b256082a398aa36036e
ayesha-ghani098 <ayeshaghanichi@gmail.com>
2020-07-23 21:23:23 +0500

    chapter 49--52 complete

--- chapter 49--52/app.js
+++ chapter 49--52/app.js
@@ -19,3 +19,19 @@ function readmore() {
     para.style.visibility = "visible";
     btn.style.visibility = "hidden";
 }
+
+var display2 = document.getElementById('displayform2');
+// Applicant details in Tabular Form//
+function submit2() {
+    var name = document.getElementById('name2').value;
+    console.log(name);
+    var email = document.getElementById('email2').value;
+    var t = document.getElementById('table');
+    var row = t.insertRow(0);
+    var cell1 = row.insertCell(0);
+    var cell2 = row.insertCell(1);
+    cell1.innerHTML = name;
+    cell2.innerHTML = email;
+
+
+}
